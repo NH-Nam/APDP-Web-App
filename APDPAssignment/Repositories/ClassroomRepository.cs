28c2b2fb8f86661d799e8056ee2e8930bba7e24f
HaiNam <tungnamhn5@gmail.com>
2025-03-23 16:09:24 +0700

    Create AcademicRecordsRepository, IAcademicRecordsRepository, ClassroomRepository and IClassrooomRepository.

---
+++ APDPAssignment/Repositories/ClassroomRepository.cs
@@ -0,0 +1,72 @@
+﻿using APDPAssignment.Data;
+using APDPAssignment.Models;
+
+namespace APDPAssignment.Repositories
+{
+    public class ClassroomRepository : IClassroomRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClassroomRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Classroom> Classroom => _context.Classroom.ToList();
+
+        public Classroom GetClassroomById(int classroomId)
+        {
+            try
+            {
+                return _context.Classroom.Find(classroomId);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public bool AddClassroom(Classroom classroom)
+        {
+            try
+            {
+                _context.Classroom.Add(classroom);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateClassroom(Classroom classroom)
+        {
+            try
+            {
+                _context.Classroom.Update(classroom);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteClassroom(int classroomId)
+        {
+            try
+            {
+                var classroom = _context.Classroom.Find(classroomId);
+                _context.Classroom.Remove(classroom);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ APDPAssignment/Repositories/IClassroomRepository.cs
@@ -0,0 +1,13 @@
+﻿using APDPAssignment.Models;
+
+namespace APDPAssignment.Repositories
+{
+    public interface IClassroomRepository
+    {
+        IEnumerable<Classroom> Classroom { get; }
+        Classroom GetClassroomById(int classroomId);
+        bool AddClassroom(Classroom classroom);
+        bool UpdateClassroom(Classroom classroom);
+        bool DeleteClassroom(int classroomId);
+    }
+}
