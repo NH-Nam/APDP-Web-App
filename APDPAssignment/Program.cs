a2b94ebab1eac7944056dd6c3b00f47748cf4931
HaiNam <tungnamhn5@gmail.com>
2025-03-23 19:04:48 +0700

    Register all repositories into Program.cs

--- APDPAssignment/Program.cs
+++ APDPAssignment/Program.cs
@@ -1,4 +1,5 @@
 using APDPAssignment.Data;
+using APDPAssignment.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,18 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+builder.Services.AddScoped<ICourseRepository, CourseRepository>();
+builder.Services.AddScoped<IClassroomRepository, ClassroomRepository>();
+builder.Services.AddScoped<IScheduleRepository, ScheduleRepository>();
+builder.Services.AddScoped<IEnrollmentListRepository, EnrollmentListRepository>();
+builder.Services.AddScoped<ILecturerRepository, LecturerRepository>();
+builder.Services.AddScoped<IAdminRepository, AdminRepository>();
+builder.Services.AddScoped<IRolesRepository, RolesRepository>();
+builder.Services.AddScoped<IAcademicRecordsRepository, AcademicRecordsRepository>();
+builder.Services.AddScoped<ISemesterRepository, SemesterRepository>();
+
 
 var app = builder.Build();
 
