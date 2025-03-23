8deec3d2d055f7fb0b89e36a2e839b6f67c103ec
HaiNam <tungnamhn5@gmail.com>
2025-03-23 15:48:57 +0700

    Create a CourseRepository and ICourseRepository

---
+++ APDPAssignment/Repositories/CourseRepository.cs
@@ -0,0 +1,82 @@
+﻿using APDPAssignment.Data;
+using APDPAssignment.Models;
+
+namespace APDPAssignment.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CourseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Course> GetAllCourses()
+        {
+            try
+            {
+                return _context.Course.ToList();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public Course GetCourseById(int courseId)
+        {
+            try
+            {
+                return _context.Course.Find(courseId);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public bool AddCourse(Course course)
+        {
+            try
+            {
+                _context.Course.Add(course);
+                _context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateCourse(Course course)
+        {
+            try
+            {
+                _context.Course.Update(course);
+                _context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteCourse(int courseId)
+        {
+            try
+            {
+                var course = _context.Course.Find(courseId);
+                _context.Course.Remove(course);
+                _context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ APDPAssignment/Repositories/ICourseRepository.cs
@@ -0,0 +1,13 @@
+﻿using APDPAssignment.Models;
+
+namespace APDPAssignment.Repositories
+{
+    public interface ICourseRepository
+    {
+        IEnumerable<Course> GetAllCourses();
+        Course GetCourseById(int courseId);
+        bool AddCourse(Course course);
+        bool UpdateCourse(Course course);
+        bool DeleteCourse(int courseId);
+    }
+}
